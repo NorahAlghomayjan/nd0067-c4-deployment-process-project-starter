624ed94b99b794356029a1ad9a899859737eb0fa
NorahAlghomayjan <noor.workspace@gmail.com>
2023-01-01 02:03:11 +0300

    updates

---
+++ udagram/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=Norah
+export POSTGRES_PASSWORD=Norah1234
+export POSTGRES_HOST=database-2.cdubabhratqm.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=project
+export AWS_BUCKET=arn:aws:s3:::myproject4212
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=AKIA22BQFPE2ADAFIZWG
+export URL=http://localhost:8100
