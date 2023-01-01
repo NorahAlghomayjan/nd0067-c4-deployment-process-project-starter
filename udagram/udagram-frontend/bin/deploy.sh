d30809b283d0ca75e06d720f7fd5a6aba5850754
NorahAlghomayjan <noor.workspace@gmail.com>
2023-01-02 00:34:11 +0300

    update deeploy.sh to copy filees in src to bucket

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./src s3://elasticbeanstalk-us-east-1-743130364212/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./src/index.html s3://elasticbeanstalk-us-east-1-743130364212/
+aws s3 cp --recursive --acl public-read ./src s3://myproject4212/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./src/index.html s3://myproject4212/
